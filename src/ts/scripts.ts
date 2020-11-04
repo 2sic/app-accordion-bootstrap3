a2c1843a54b610cc58ddd63c819ae50ed17428ec
Roman Opalko <roman.opalko@2sic.com>
2020-11-04 15:50:39 +0100

    refactoring scroll and hash param

--- src/ts/scripts.ts
+++ src/ts/scripts.ts
@@ -1,27 +1,27 @@
 $(function() {
+  var navHeight = $('header').height();
 	// attach click to all accordions when loading
 	$('.co-accordion-title').click(function(e){
 		e.preventDefault();
-    var _this = $(this);
-    var navHeight = 119;
 
     // add hash to url
-		location.hash = $(this).attr('id');
+		location.hash = $(this).data('hash');
 
-    $(this).toggleClass('active')
-      .next('.co-accordion-content').slideToggle('normal', function(){
-        $(window).resize();
-        $('html, body').animate({
-          scrollTop: _this.offset().top - navHeight
-        }, 500);
-      });
+    $(this).toggleClass('active').next('.co-accordion-content').slideToggle('normal');
 	});
 
 	// get hash from url and open specific item
 	if(window.location.hash){
-		var hash = window.location.hash;
-		if($(hash).length > 0){
-      $(hash).click();
+    var hash = window.location.hash.replace('#', '');
+    var targetHashElem = $(`#hash-${hash}`);
+		
+		if(targetHashElem.length > 0){
+      targetHashElem.click();
+      $('html, body').animate({
+        scrollTop: targetHashElem.offset().top - navHeight 
+      }, 500, function() {
+				targetHashElem.addClass('active').next('.co-accordion-content').slideDown('normal')
+			}); 
 		}
 	}
 });
